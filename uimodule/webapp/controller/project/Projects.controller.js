846214368e87b4b0ef5d4280ef9194b98ae16000
EGO <elias.golam@brainsystem.eu>
2025-01-27 15:36:07 +0100

    Implemented projects filter list

--- uimodule/webapp/controller/project/Projects.controller.js
+++ uimodule/webapp/controller/project/Projects.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define(
-  ['eligolam/boldbase/controller/BaseControllerProject', 'eligolam/boldbase/Modules/api', 'eligolam/boldbase/model/Formatter', 'sap/ui/model/json/JSONModel'],
-  function (BaseController, api, Formatter, JSONModel) {
+  ['eligolam/boldbase/controller/BaseControllerProject', 'eligolam/boldbase/Modules/api', 'eligolam/boldbase/model/Formatter', 'sap/ui/model/json/JSONModel', "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"],
+  function (BaseController, api, Formatter, JSONModel, Filter, FilterOperator) {
     'use strict'
 
     return BaseController.extend('eligolam.boldbase.controller.project.Projects', {
@@ -15,10 +16,40 @@ sap.ui.define(
       _onRouteMatched(oEvent) { this.getProjects() },
       //#endregion Setup
 
+      //#region Data
       async getProjects() {
         let data = await api.asyncGet('mock/data/projects.json')
         this.setModel(new JSONModel(data), 'Projects')
-      }
+      },
+      //#endregion Data
+
+      //#region DOM Functions
+
+      //#region Search
+      onProjectSearch(oEvent) {
+        const sQuery = (oEvent.getSource().getValue() ?? '').trim()
+        this.filterList(sQuery)
+      },
+
+      filterList(sQuery) {
+        const oBinding = this.getView().byId('ProjectsList').getBinding('items')
+
+        if (!sQuery || sQuery.length == 0) {
+          oBinding.filter(null)
+          return // Guard Statement if no search string is given
+        }
+
+        oBinding.filter(new Filter({
+          filters: [
+            new Filter('ProjectName', FilterOperator.Contains, sQuery),
+            new Filter('ClientName', FilterOperator.Contains, sQuery)
+          ],
+          and: false
+        }))
+      },
+      //#endregion Search
+
+      //#endregion DOM Functions
     })
   }
 )
